89e613024985375af5538314d0a9b8c105c9daac
Niyolei <113349382+Niyolei@users.noreply.github.com>
2023-12-15 16:03:30 +0000

    Hades Dialog Part
    Dialog code for the hades dialog

--- Assets/Quick Time Events/Scripts/Quick Time Events/QT_Hades.cs
+++ Assets/Quick Time Events/Scripts/Quick Time Events/QT_Hades.cs
@@ -13,6 +13,7 @@ namespace DefaultNamespace
         
         private bool wasShown = false;
         private bool toShow = true;
+        private bool dialog = true;
         
 
         [Header("Keys")]
@@ -24,6 +25,29 @@ namespace DefaultNamespace
         private UnityEngine.UI.Image sImage;
         [SerializeField]
         private UnityEngine.UI.Image dImage;
+
+        [Header("First Dialog")] 
+        [SerializeField]
+        private Audio_Data sissyOne;
+        [SerializeField]
+        private Audio_Data sissyTwo;
+        [SerializeField]
+        private Audio_Data hades;
+        
+        [Header("Second Dialog")]
+        [SerializeField]
+        private Audio_Data sissyThree;
+        [SerializeField]
+        private Audio_Data hadesTwo;
+        
+        [Header("Third Dialog")]
+        [SerializeField]
+        private Audio_Data sissyFour;
+        [SerializeField]
+        private Audio_Data sissyFive;
+        [SerializeField]
+        private Audio_Data hadesThree;
+        
         
         
         
@@ -41,6 +65,23 @@ namespace DefaultNamespace
         protected override void ConfirmGuide()
         {
             base.ConfirmGuide();
+            if (dialog)
+            {
+                int p = Random.Range(0, 3);
+                switch (p)
+                {
+                    case 0:
+                        StartCoroutine(DialogOne());
+                        break;
+                    case 1:
+                        StartCoroutine(DialogTwo());
+                        break;
+                    case 2:
+                        StartCoroutine(DialogThree());
+                        break;
+                }
+                dialog = false;
+            }
             if (toShow && IsRunning)
             {
                 StartCoroutine(ShowKyes());
@@ -111,6 +152,32 @@ namespace DefaultNamespace
         {
             image.gameObject.transform.localScale = new Vector3(0, 0, 0);
             yield return new WaitForSeconds(0.3f);
+        }
+        
+        private IEnumerator DialogOne()
+        {
+            audioData.audioEvents.playSound.Raise(sissyOne);
+            yield return new WaitForSeconds(sissyOne.clip[0].length);
+            audioData.audioEvents.playSound.Raise(hades);
+            yield return new WaitForSeconds(hades.clip[0].length);
+            audioData.audioEvents.playSound.Raise(sissyTwo);
+        }
+        
+        private IEnumerator DialogTwo()
+        {
+            audioData.audioEvents.playSound.Raise(sissyThree);
+            yield return new WaitForSeconds(sissyThree.clip[0].length);
+            audioData.audioEvents.playSound.Raise(hadesTwo);
+        }
+        
+        private IEnumerator DialogThree()
+        {
+            audioData.audioEvents.playSound.Raise(sissyFour);
+            yield return new WaitForSeconds(sissyFour.clip[0].length);
+            audioData.audioEvents.playSound.Raise(hadesThree);
+            yield return new WaitForSeconds(hadesThree.clip[0].length);
+            audioData.audioEvents.playSound.Raise(sissyFive);
+            
         }
     }
 }
